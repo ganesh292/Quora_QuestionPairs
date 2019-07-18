ef904f3ac2c51b3976977697af828e45c6cf5003
ganesh <ganesh292@gmail.com>
2019-07-18 02:52:18 -0400

    Train-test split

--- main.py
+++ main.py
@@ -34,8 +34,7 @@ from sklearn.decomposition import PCA
 import nltk
 nltk.download('punkt')
 from nltk.tokenize import word_tokenize
-
-
+from sklearn.model_selection import train_test_split
 from keras import backend as K
 from keras import callbacks
 import tensorflow as tf
@@ -338,26 +337,28 @@ def main():
   # df_sub = df_sub.reindex(np.random.permutation(df_sub.index))
   features = add_features()
   # set number of train and test instances
-  num_train = int(df_sub.shape[0] * 0.70)
-  num_val = int(df_sub.shape[0] * 0.10)
-  num_test = df_sub.shape[0] - num_train - num_val               
-  print("Number of training pairs: %i"%(num_train))
-  print("Number of Validation pairs: %i"%(num_val))
-  print("Number of testing pairs: %i"%(num_test))
+
+  total = np.arange(df_sub.shape[0])
+  num_train1,num_test = train_test_split(total,test_size=0.1)
+  num_train,num_val = train_test_split(num_train1,test_size=1/9)
+              
+  print("Number of training pairs: %i"%(len(num_train)))
+  print("Number of Validation pairs: %i"%(len(num_val)))
+  print("Number of testing pairs: %i"%(len(num_test)))
 
 
   # init data data arrays
-  X_train_cnn_a = np.zeros([num_train, 324, 3])
-  X_test_cnn_a  = np.zeros([num_test, 324, 3])
-  X_val_cnn_a  = np.zeros([num_val, 324, 3])
+  X_train_cnn_a = np.zeros([len(num_train), 324, 3])
+  X_test_cnn_a  = np.zeros([len(num_test), 324, 3])
+  X_val_cnn_a  = np.zeros([len(num_val), 324, 3])
 
-  X_train_cnn_b = np.zeros([num_train, 324, 3])
-  X_test_cnn_b  = np.zeros([num_test, 324, 3])
-  X_val_cnn_b  = np.zeros([num_val, 324, 3])
+  X_train_cnn_b = np.zeros([len(num_train), 324, 3])
+  X_test_cnn_b  = np.zeros([len(num_test), 324, 3])
+  X_val_cnn_b  = np.zeros([len(num_val), 324, 3])
 
-  Y_train = np.zeros([num_train]) 
-  Y_test = np.zeros([num_test])
-  Y_val = np.zeros([num_val]) 
+  Y_train = np.zeros([len(num_train)]) 
+  Y_test = np.zeros([len(num_test)])
+  Y_val = np.zeros([len(num_val)]) 
 
   # format data 
   # b = [a[None,:] for a in list(df_sub['q1_feats'].values)]
@@ -367,43 +368,42 @@ def main():
   # q2_feats = np.concatenate(b, axis=0)
 
 
-  num_val = num_train + int(df_sub.shape[0] * 0.10)
   # fill data arrays with features
-  X_train_cnn_a[:,:,0] = ft_emb_q1[:num_train]
-  X_train_cnn_a[:,:,1] = w2v_emb_q1[:num_train]
-  X_train_cnn_a[:,:,2] = glove_emb_q1[:num_train]
+  X_train_cnn_a[:,:,0] = ft_emb_q1[num_train]
+  X_train_cnn_a[:,:,1] = w2v_emb_q1[num_train]
+  X_train_cnn_a[:,:,2] = glove_emb_q1[num_train]
 
-  X_train_cnn_b[:,:,0] = ft_emb_q2[:num_train]
-  X_train_cnn_b[:,:,1] = w2v_emb_q2[:num_train]
-  X_train_cnn_b[:,:,2] = glove_emb_q2[:num_train]
+  X_train_cnn_b[:,:,0] = ft_emb_q2[num_train]
+  X_train_cnn_b[:,:,1] = w2v_emb_q2[num_train]
+  X_train_cnn_b[:,:,2] = glove_emb_q2[num_train]
 
-  features_train = features[:num_train]
-  features_b_train = bert_q[:num_train]
+  features_train = features[num_train]
+  features_b_train = bert_q[num_train]
   Y_train = df_sub[:num_train]['is_duplicate'].values
 
-  X_val_cnn_a[:,:,0] = ft_emb_q1[num_train:num_val]
-  X_val_cnn_a[:,:,1] = w2v_emb_q1[num_train:num_val]
-  X_val_cnn_a[:,:,2] = glove_emb_q1[num_train:num_val]
+  X_val_cnn_a[:,:,0] = ft_emb_q1[num_val]
+  X_val_cnn_a[:,:,1] = w2v_emb_q1[num_val]
+  X_val_cnn_a[:,:,2] = glove_emb_q1[num_val]
 
-  X_val_cnn_b[:,:,0] = ft_emb_q2[num_train:num_val]
-  X_val_cnn_b[:,:,1] = w2v_emb_q2[num_train:num_val]
-  X_val_cnn_b[:,:,2] = glove_emb_q2[num_train:num_val]
+  X_val_cnn_b[:,:,0] = ft_emb_q2[num_val]
+  X_val_cnn_b[:,:,1] = w2v_emb_q2[num_val]
+  X_val_cnn_b[:,:,2] = glove_emb_q2[num_val]
 
-  features_val = features[num_train:num_val]
-  features_b_val = bert_q[num_train:num_val]
-  Y_val = df_sub[num_train:num_val]['is_duplicate'].values
+  features_val = features[num_val]
+  features_b_val = bert_q[num_val]
+  Y_val = df_sub[num_val]['is_duplicate'].values
 
               
-  X_test_cnn_a[:,:,0] = ft_emb_q1[num_val:]
-  X_test_cnn_a[:,:,1] = w2v_emb_q1[num_val:]
-  X_test_cnn_a[:,:,2] = glove_emb_q1[num_val:]
+  X_test_cnn_a[:,:,0] = ft_emb_q1[num_val]
+  X_test_cnn_a[:,:,1] = w2v_emb_q1[num_val]
+  X_test_cnn_a[:,:,2] = glove_emb_q1[num_val]
 
-  X_test_cnn_b[:,:,0] = ft_emb_q2[num_val:]
-  X_test_cnn_b[:,:,1] = w2v_emb_q2[num_val:]
-  X_test_cnn_b[:,:,2] = glove_emb_q2[num_val:]
-  features_test = features[num_val:]
-  features_b_test = bert_q[num_val:]
-  Y_test = df_sub[num_val:]['is_duplicate'].values
+  X_test_cnn_b[:,:,0] = ft_emb_q2[num_val]
+  X_test_cnn_b[:,:,1] = w2v_emb_q2[num_val]
+  X_test_cnn_b[:,:,2] = glove_emb_q2[num_val]
+  features_test = features[num_val]
+  features_b_test = bert_q[num_val]
+  Y_test = df_sub[num_val]['is_duplicate'].values
 
 
   
@@ -508,7 +508,7 @@ def main():
               validation_data=([X_val_cnn_a, X_val_cnn_b,X_val_lstm1_a, X_val_lstm1_b,
                               X_val_lstm2_a, X_val_lstm2_b,X_val_lstm3_a, X_val_lstm3_b,features_val,features_b_val]
                               , Y_val),
-              batch_size=384, nb_epoch=8, shuffle=True,callbacks = callbacks_list)
+              batch_size=384, nb_epoch=16, shuffle=True,callbacks = callbacks_list)
 
     score = net.evaluate([X_test_cnn_a, X_test_cnn_b,X_test_lstm1_a, X_test_lstm1_b,
                   X_test_lstm2_a, X_test_lstm2_b,X_test_lstm3_a, X_test_lstm3_b,features_test,features_b_test],Y_test,batch_size=384)
