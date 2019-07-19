cf96920be6a084ded3d2e5701cab41c248a6d55e
ganesh292 <ganesh292@gmail.com>
2019-07-18 22:25:48 -0400

    Changes for BERT Training

--- main.py
+++ main.py
@@ -60,15 +60,7 @@ import numpy as np
 import torch
 from models import InferSent
 from keras import regularizers
-# import matplotlib as mpl
-# # %matplotlib inline
-# from matplotlib import pyplot as plt
-# from keras.utils import plot_model 
-# from IPython.display import Image
-# plot_model(net, to_file='model_upgraded_1.png', show_layer_names=True)
-# Image('model_upgraded_1.png')
-# from google.colab import files
-# files.download( "model_upgraded_1.png" )
+
 
 
 
@@ -137,7 +129,8 @@ def create_base_network_cnn(input_dimensions):
   pool2  = MaxPooling1D(pool_size=1,strides=1,name='pool2')(conv2)
   conv3  = Conv1D(filters=128,kernel_size=4,strides=1,activation='relu',name='conv3')(pool2)
   flat   = Flatten(name='flat_cnn')(conv3)
-  dense  = Dense(376,name='dense_cnn')(flat)
+  # dense  = Dense(376,name='dense_cnn')(flat)
+  dense  = Dense(300,name='dense_cnn',kernel_regularizer=regularizers.l2(0.01))(flat)
    
   model  = Model(input=input,output=dense)
   return model
@@ -146,7 +139,8 @@ def create_base_network_lstm(input_dimensions):
   input = Input(shape=(input_dimensions[0],1))
   layer1 = LSTM(20, return_sequences=True,activation='relu',name='lstm_1')(input)
   layer2 = LSTM(20,return_sequences=False,activation='relu',name='lstm_2')(layer1)
-  dense = Dense(376,name='dense_lstm')(layer2)
+  # dense = Dense(376,name='dense_lstm')(layer2)
+  dense = Dense(768,name='dense_lstm')(layer2)
   
   model = Model(input=input,output=dense)
   return model
@@ -175,73 +169,91 @@ def eucl_dist_output_shape(shapes):
     
 def add_features():
   if sys.argv[1] == "b":
-    data_features = pd.read_csv("quora_features_balanced.csv")
+    data_features = pd.read_csv("quora_features_BERT_balanced.csv")
   else:
-    data_features = pd.read_csv("quora_features_unbalanced.csv")
+    data_features = pd.read_csv("quora_features_BERT_unbalanced.csv")
   
   features = data_features.drop(['question1', 'question2', 'is_duplicate','jaccard_distance'],axis=1).values
   print('Shape of Features added',features.shape)
   return features
 
 def create_network(input_dimensions,num_features):
-  base_network_lstm_1 = create_base_network_lstm(input_dimensions)
-  base_network_lstm_2 = create_base_network_lstm(input_dimensions)
-  base_network_lstm_3 = create_base_network_lstm(input_dimensions)
+
+  # #Fasttext
+  # base_network_lstm_1 = create_base_network_lstm(input_dimensions)
+  # input_a_lstm_1 = Input(shape=(input_dimensions[0],1))
+  # input_b_lstm_1 = Input(shape=(input_dimensions[0],1))
+  # # LSTM with embedding 1
+  # inter_a_lstm_1 = base_network_lstm_1(input_a_lstm_1)
+  # inter_b_lstm_1 = base_network_lstm_1(input_b_lstm_1)
+  # d_lstm_1 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([inter_a_lstm_1, inter_b_lstm_1])
+
+
+  # #W2V
+  # base_network_lstm_2 = create_base_network_lstm(input_dimensions)
+  # input_a_lstm_2 = Input(shape=(input_dimensions[0],1))
+  # input_b_lstm_2 = Input(shape=(input_dimensions[0],1))
+  # # LSTM with embedding 2
+  # inter_a_lstm_2 = base_network_lstm_2(input_a_lstm_2)
+  # inter_b_lstm_2 = base_network_lstm_2(input_b_lstm_2)
+  # d_lstm_2 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([inter_a_lstm_2, inter_b_lstm_2])
+
+
+  # #Glove
+  # base_network_lstm_3 = create_base_network_lstm(input_dimensions)
+  # input_a_lstm_3 = Input(shape=(input_dimensions[0],1))
+  # input_b_lstm_3 = Input(shape=(input_dimensions[0],1))
+  # # LSTM with embedding 3
+  # inter_a_lstm_3 = base_network_lstm_3(input_a_lstm_3)
+  # inter_b_lstm_3 = base_network_lstm_3(input_b_lstm_3)
+  # d_lstm_3 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([inter_a_lstm_3, inter_b_lstm_3])
+
+  #BERT
+  base_network_lstm_4 = create_base_network_lstm(input_dimensions)
+  input_a_lstm_4 = Input(shape=(768,1))
+  input_b_lstm_4 = Input(shape=(768,1))
+   # LSTM with embedding 3
+  inter_a_lstm_4 = base_network_lstm_4(input_a_lstm_4)
+  inter_b_lstm_4 = base_network_lstm_4(input_b_lstm_4)
+
+  
+  #CNN
   base_network_cnn = create_base_network_cnn(input_dimensions)
-  
-  
+  # CNN with 3 channel embedding
   input_a_cnn = Input(shape=(input_dimensions[0],input_dimensions[1]))
   input_b_cnn = Input(shape=(input_dimensions[0],input_dimensions[1]))
+  inter_a_cnn = base_network_cnn(input_a_cnn)
+  inter_b_cnn = base_network_cnn(input_b_cnn)
+
+
+
+  d_cnn = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([inter_a_cnn, inter_b_cnn])
+  # d_lstm_1 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([inter_a_lstm_1, inter_b_lstm_1])
+  # d_lstm_2 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([inter_a_lstm_2, inter_b_lstm_2])
+  # d_lstm_3 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([inter_a_lstm_3, inter_b_lstm_3])
+  d_lstm_4 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([inter_a_lstm_4, inter_b_lstm_4])
   
-  input_a_lstm_1 = Input(shape=(input_dimensions[0],1))
-  input_b_lstm_1 = Input(shape=(input_dimensions[0],1))
-                         
-  input_a_lstm_2 = Input(shape=(input_dimensions[0],1))
-  input_b_lstm_2 = Input(shape=(input_dimensions[0],1))
-                         
-  input_a_lstm_3 = Input(shape=(input_dimensions[0],1))
-  input_b_lstm_3 = Input(shape=(input_dimensions[0],1))
-  
-  # Features
+  # Additional Features from Thakur (BERT)
   features = Input(shape=(num_features,))
+
+  #BERT itself
   features_b = Input(shape=(768,))
   
   
-  # CNN with 3 channel embedding
-  inter_a_cnn = base_network_cnn(input_a_cnn)
-  inter_b_cnn = base_network_cnn(input_b_cnn)
-  
-  # LSTM with embedding 1
-  inter_a_lstm_1 = base_network_lstm_1(input_a_lstm_1)
-  inter_b_lstm_1 = base_network_lstm_1(input_b_lstm_1)
-  
-  # LSTM with embedding 2
-  inter_a_lstm_2 = base_network_lstm_2(input_a_lstm_2)
-  inter_b_lstm_2 = base_network_lstm_2(input_b_lstm_2)
-  
-  # LSTM with embedding 3
-  inter_a_lstm_3 = base_network_lstm_3(input_a_lstm_3)
-  inter_b_lstm_3 = base_network_lstm_3(input_b_lstm_3)
-  
-  
-  d_cnn = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([inter_a_cnn, inter_b_cnn])
-  d_lstm_1 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([inter_a_lstm_1, inter_b_lstm_1])
-  d_lstm_2 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([inter_a_lstm_2, inter_b_lstm_2])
-  d_lstm_3 = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([inter_a_lstm_3, inter_b_lstm_3])
-  
-  
-  feature_set = Concatenate(axis=-1)([d_cnn,d_lstm_1,d_lstm_2,d_lstm_3,features,features_b])
+  #Concatenation of Features
+  # feature_set = Concatenate(axis=-1)([d_cnn,d_lstm_1,d_lstm_2,d_lstm_3,features,features_b])
+  feature_set = Concatenate(axis=-1)([d_cnn,d_lstm_4,features,features_b])
   # feature_set = add_features(feature_set)
+
+  #Final Dense Layer
   d1 = Dense(128, activation='relu',kernel_regularizer=regularizers.l2(0.01))(feature_set)
   drop1 = Dropout(0.2)(d1)
   d2 = Dense(128, activation='relu',kernel_regularizer=regularizers.l2(0.01))(drop1)
   drop2 = Dropout(0.2)(d2)
   d3 = Dense(1, activation='sigmoid')(drop2)
-  
-  
-  #value = dense_network(feature_set)
-  
-  model = Model(input=[input_a_cnn, input_b_cnn , input_a_lstm_1, input_b_lstm_1, input_a_lstm_2, input_b_lstm_2, input_a_lstm_3, input_b_lstm_3,features,features_b], output=d3)
+
+  model = Model(input=[input_a_cnn, input_b_cnn , input_a_lstm_4, input_b_lstm_4,features,features_b], output=d3)
+  # model = Model(input=[input_a_cnn, input_b_cnn , input_a_lstm_1, input_b_lstm_1, input_a_lstm_2, input_b_lstm_2, input_a_lstm_3, input_b_lstm_3,features,features_b], output=d3)
   print("Model Architecture Designed")
   return model
   
@@ -304,6 +316,11 @@ def main():
       print('Loading Embeddings BERT')
       bert_q = genfromtxt('/tmp/Ganesh_MSCI/Unbalanced_Embeddings/bert/bert_qpair_unbalanced.csv', delimiter=',',skip_header=1)
       bert_q = np.delete(bert_q,0,1)
+      bert_q1 = genfromtxt('/tmp/Ganesh_MSCI/Unbalanced_Embeddings/bert/bert_q1_unbalanced.csv', delimiter=',',skip_header=1)
+      bert_q1 = np.delete(bert_q1,0,1)
+      print('Loading Embeddings BERTQ2')
+      bert_q2 = genfromtxt('/tmp/Ganesh_MSCI/Unbalanced_Embeddings/bert/bert_q2_unbalanced.csv', delimiter=',',skip_header=1)
+      bert_q2 = np.delete(bert_q2,0,1)
       
     elif sys.argv[1] == "b":
       print('Loading Embeddings W2vec')
@@ -328,7 +345,12 @@ def main():
       print('Loading Embeddings BERT')
       bert_q = genfromtxt('/tmp/Ganesh_MSCI/balanced_Embeddings/bert/bert_qpair_balanced.csv', delimiter=',',skip_header=1)
       bert_q = np.delete(bert_q,0,1)
-      
+      print('Loading Embeddings BERTQ1')
+      bert_q1 = genfromtxt('/tmp/Ganesh_MSCI/balanced_Embeddings/bert/bert_q1_balanced.csv', delimiter=',',skip_header=1)
+      bert_q1 = np.delete(bert_q1,0,1)
+      print('Loading Embeddings BERTQ2')
+      bert_q2 = genfromtxt('/tmp/Ganesh_MSCI/balanced_Embeddings/bert/bert_q2_balanced.csv', delimiter=',',skip_header=1)
+      bert_q2 = np.delete(bert_q2,0,1)
 
   # print("Getting Bert Embeddings..")
   # bert_e = get_bertembeddings(q1sents,q2sents)
@@ -360,15 +382,20 @@ def main():
   Y_test = np.zeros([len(num_test)])
   Y_val = np.zeros([len(num_val)]) 
 
-  # format data 
-  # b = [a[None,:] for a in list(df_sub['q1_feats'].values)]
-  # q1_feats = np.concatenate(b, axis=0)
+  #Labels
+  Y_train = df_sub[:num_train]['is_duplicate'].values
+  Y_val = df_sub[num_val]['is_duplicate'].values
+  Y_test = df_sub[num_val]['is_duplicate'].values
+  
+  #Features Data
+  features_train = features[num_train]
+  features_b_train = bert_q[num_train]
+  features_val = features[num_val]
+  features_b_val = bert_q[num_val]
+  features_test = features[num_val]
+  features_b_test = bert_q[num_val]
 
-  # b = [a[None,:] for a in list(df_sub['q2_feats'].values)]
-  # q2_feats = np.concatenate(b, axis=0)
-
-
-  # fill data arrays with features
+  #CNN DATA
   X_train_cnn_a[:,:,0] = ft_emb_q1[num_train]
   X_train_cnn_a[:,:,1] = w2v_emb_q1[num_train]
   X_train_cnn_a[:,:,2] = glove_emb_q1[num_train]
@@ -377,10 +404,7 @@ def main():
   X_train_cnn_b[:,:,1] = w2v_emb_q2[num_train]
   X_train_cnn_b[:,:,2] = glove_emb_q2[num_train]
 
-  features_train = features[num_train]
-  features_b_train = bert_q[num_train]
-  Y_train = df_sub[:num_train]['is_duplicate'].values
-
+  
   X_val_cnn_a[:,:,0] = ft_emb_q1[num_val]
   X_val_cnn_a[:,:,1] = w2v_emb_q1[num_val]
   X_val_cnn_a[:,:,2] = glove_emb_q1[num_val]
@@ -389,10 +413,7 @@ def main():
   X_val_cnn_b[:,:,1] = w2v_emb_q2[num_val]
   X_val_cnn_b[:,:,2] = glove_emb_q2[num_val]
 
-  features_val = features[num_val]
-  features_b_val = bert_q[num_val]
-  Y_val = df_sub[num_val]['is_duplicate'].values
-
+  
               
   X_test_cnn_a[:,:,0] = ft_emb_q1[num_val]
   X_test_cnn_a[:,:,1] = w2v_emb_q1[num_val]
@@ -401,10 +422,7 @@ def main():
   X_test_cnn_b[:,:,0] = ft_emb_q2[num_val]
   X_test_cnn_b[:,:,1] = w2v_emb_q2[num_val]
   X_test_cnn_b[:,:,2] = glove_emb_q2[num_val]
-  features_test = features[num_val]
-  features_b_test = bert_q[num_val]
-  Y_test = df_sub[num_val]['is_duplicate'].values
-
+  
 
   
 
@@ -422,62 +440,76 @@ def main():
   print(net.summary())
 
 
-  # Training Sets for LSTMS
+  # # Training Sets for LSTM1
 
-  X_intera_train_1 = X_train_cnn_a[:,:,0]
-  X_interb_train_1 = X_train_cnn_b[:,:,0]
-  X_train_lstm1_a = X_intera_train_1[:,:,np.newaxis]
-  X_train_lstm1_b = X_interb_train_1[:,:,np.newaxis]
+  # X_intera_train_1 = X_train_cnn_a[:,:,0]
+  # X_interb_train_1 = X_train_cnn_b[:,:,0]
+  # X_train_lstm1_a = X_intera_train_1[:,:,np.newaxis]
+  # X_train_lstm1_b = X_interb_train_1[:,:,np.newaxis]
 
-  X_intera_train_2 = X_train_cnn_a[:,:,1]
-  X_interb_train_2 = X_train_cnn_b[:,:,1]
-  X_train_lstm2_a = X_intera_train_2[:,:,np.newaxis]
-  X_train_lstm2_b = X_interb_train_2[:,:,np.newaxis]
+  # X_intera_val_1 = X_val_cnn_a[:,:,0]
+  # X_interb_val_1 = X_val_cnn_b[:,:,0]
+  # X_val_lstm1_a = X_intera_val_1[:,:,np.newaxis]
+  # X_val_lstm1_b = X_interb_val_1[:,:,np.newaxis]
 
-  X_intera_train_3 = X_train_cnn_a[:,:,2]
-  X_interb_train_3 = X_train_cnn_b[:,:,2]
-  X_train_lstm3_a = X_intera_train_3[:,:,np.newaxis]
-  X_train_lstm3_b = X_interb_train_3[:,:,np.newaxis]
+  # X_intera_test_1 = X_test_cnn_a[:,:,0]
+  # X_interb_test_1 = X_test_cnn_b[:,:,0]
+  # X_test_lstm1_a = X_intera_test_1[:,:,np.newaxis]
+  # X_test_lstm1_b = X_interb_test_1[:,:,np.newaxis]
 
-  # Validation Sets for LSTMS
 
-  X_intera_val_1 = X_val_cnn_a[:,:,0]
-  X_interb_val_1 = X_val_cnn_b[:,:,0]
-  X_val_lstm1_a = X_intera_val_1[:,:,np.newaxis]
-  X_val_lstm1_b = X_interb_val_1[:,:,np.newaxis]
+  # # Validation Sets for LSTM2
 
-  X_intera_val_2 = X_val_cnn_a[:,:,1]
-  X_interb_val_2 = X_val_cnn_b[:,:,1]
-  X_val_lstm2_a = X_intera_val_2[:,:,np.newaxis]
-  X_val_lstm2_b = X_interb_val_2[:,:,np.newaxis]
+  # X_intera_train_2 = X_train_cnn_a[:,:,1]
+  # X_interb_train_2 = X_train_cnn_b[:,:,1]
+  # X_train_lstm2_a = X_intera_train_2[:,:,np.newaxis]
+  # X_train_lstm2_b = X_interb_train_2[:,:,np.newaxis]
 
-  X_intera_val_3 = X_val_cnn_a[:,:,2]
-  X_interb_val_3 = X_val_cnn_b[:,:,2]
-  X_val_lstm3_a = X_intera_val_3[:,:,np.newaxis]
-  X_val_lstm3_b = X_interb_val_3[:,:,np.newaxis]
+  # X_intera_val_2 = X_val_cnn_a[:,:,1]
+  # X_interb_val_2 = X_val_cnn_b[:,:,1]
+  # X_val_lstm2_a = X_intera_val_2[:,:,np.newaxis]
+  # X_val_lstm2_b = X_interb_val_2[:,:,np.newaxis]
 
-  # Test Set for LSTMS
+  # X_intera_test_2 = X_test_cnn_a[:,:,1]
+  # X_interb_test_2 = X_test_cnn_b[:,:,1]
+  # X_test_lstm2_a = X_intera_test_2[:,:,np.newaxis]
+  # X_test_lstm2_b = X_interb_test_2[:,:,np.newaxis]
+  
+  # # Test Set for LSTM3
 
-  X_intera_test_1 = X_test_cnn_a[:,:,0]
-  X_interb_test_1 = X_test_cnn_b[:,:,0]
-  X_test_lstm1_a = X_intera_test_1[:,:,np.newaxis]
-  X_test_lstm1_b = X_interb_test_1[:,:,np.newaxis]
+  # X_intera_train_3 = X_train_cnn_a[:,:,2]
+  # X_interb_train_3 = X_train_cnn_b[:,:,2]
+  # X_train_lstm3_a = X_intera_train_3[:,:,np.newaxis]
+  # X_train_lstm3_b = X_interb_train_3[:,:,np.newaxis]
+ 
+  # X_intera_val_3 = X_val_cnn_a[:,:,2]
+  # X_interb_val_3 = X_val_cnn_b[:,:,2]
+  # X_val_lstm3_a = X_intera_val_3[:,:,np.newaxis]
+  # X_val_lstm3_b = X_interb_val_3[:,:,np.newaxis]
+  
+  # X_intera_test_3 = X_test_cnn_a[:,:,2]
+  # X_interb_test_3 = X_test_cnn_b[:,:,2]
+  # X_test_lstm3_a = X_intera_test_3[:,:,np.newaxis]
+  # X_test_lstm3_b = X_interb_test_3[:,:,np.newaxis]
 
-  X_intera_test_2 = X_test_cnn_a[:,:,1]
-  X_interb_test_2 = X_test_cnn_b[:,:,1]
-  X_test_lstm2_a = X_intera_test_2[:,:,np.newaxis]
-  X_test_lstm2_b = X_interb_test_2[:,:,np.newaxis]
 
-  X_intera_test_3 = X_test_cnn_a[:,:,2]
-  X_interb_test_3 = X_test_cnn_b[:,:,2]
-  X_test_lstm3_a = X_intera_test_3[:,:,np.newaxis]
-  X_test_lstm3_b = X_interb_test_3[:,:,np.newaxis]
+  # Test Set for LSTM4 BERT
+
+
+  X_train_lstm4_a = bert_q1[num_train]
+  X_train_lstm4_b = bert_q2[num_train]
+ 
+  X_val_lstm4_a = bert_q1[num_val]
+  X_val_lstm4_b = bert_q1[num_val]
+
+  X_test_lstm4_a = bert_q1[num_test]
+  X_test_lstm4_b = bert_q1[num_test]
 
   print("Input Shapes")
   print("CNN Shape")
   print(X_train_cnn_a.shape,X_val_cnn_a.shape,X_test_cnn_a.shape)
-  print("LSTM (3) Shape:")
-  print(X_train_lstm1_a.shape,X_val_lstm1_a.shape,X_test_lstm1_a.shape)
+  print("LSTM (x3) Shape:")
+  print(X_train_lstm4_a.shape,X_val_lstm4_a.shape,X_test_lstm4_a.shape)
 
   print("Features shape:",features_train.shape,features_val.shape,features_test.shape)
   print("BERT Features shape:",features_b_train.shape,features_b_val.shape,features_b_test.shape)
@@ -501,17 +533,22 @@ def main():
 
 
 
-
-      net.fit([X_train_cnn_a, X_train_cnn_b, X_train_lstm1_a, X_train_lstm1_b,
-                X_train_lstm2_a, X_train_lstm2_b,X_train_lstm3_a, X_train_lstm3_b,features_train,features_b_train], 
+      net.fit([X_train_cnn_a, X_train_cnn_b, X_train_lstm4_a, X_train_lstm4_b,features_train,features_b_train], 
                 Y_train,
-              validation_data=([X_val_cnn_a, X_val_cnn_b,X_val_lstm1_a, X_val_lstm1_b,
-                              X_val_lstm2_a, X_val_lstm2_b,X_val_lstm3_a, X_val_lstm3_b,features_val,features_b_val]
+              validation_data=([X_val_cnn_a, X_val_cnn_b,X_val_lstm4_a, X_val_lstm4_b,features_val,features_b_val]
                               , Y_val),
               batch_size=384, nb_epoch=16, shuffle=True,callbacks = callbacks_list)
 
-    score = net.evaluate([X_test_cnn_a, X_test_cnn_b,X_test_lstm1_a, X_test_lstm1_b,
-                  X_test_lstm2_a, X_test_lstm2_b,X_test_lstm3_a, X_test_lstm3_b,features_test,features_b_test],Y_test,batch_size=384)
+      # net.fit([X_train_cnn_a, X_train_cnn_b, X_train_lstm1_a, X_train_lstm1_b,
+      #           X_train_lstm2_a, X_train_lstm2_b,X_train_lstm3_a, X_train_lstm3_b,features_train,features_b_train], 
+      #           Y_train,
+      #         validation_data=([X_val_cnn_a, X_val_cnn_b,X_val_lstm1_a, X_val_lstm1_b,
+      #                         X_val_lstm2_a, X_val_lstm2_b,X_val_lstm3_a, X_val_lstm3_b,features_val,features_b_val]
+      #                         , Y_val),
+      #         batch_size=384, nb_epoch=16, shuffle=True,callbacks = callbacks_list)
+    score = net.evaluate([X_test_cnn_a, X_test_cnn_b,X_test_lstm4_a, X_test_lstm4_b,features_test,features_b_test],Y_test,batch_size=384)
+    # score = net.evaluate([X_test_cnn_a, X_test_cnn_b,X_test_lstm1_a, X_test_lstm1_b,
+    #               X_test_lstm2_a, X_test_lstm2_b,X_test_lstm3_a, X_test_lstm3_b,features_test,features_b_test],Y_test,batch_size=384)
     print('Test loss : {:.4f}'.format(score[0]))
     print('Test accuracy : {:.4f}'.format(score[1]))
   else:
